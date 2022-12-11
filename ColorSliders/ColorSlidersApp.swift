d9a3d98717fdefa9b468fcbaca10a0592f7c1018
Alexandr-Mayyura <sonickill@yandex.ru>
2022-12-11 22:12:42 +0300

    Initial Commit

---
+++ ColorSliders/ColorSlidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSlidersApp.swift
+//  ColorSliders
+//
+//  Created by Aleksandr Mayyura on 11.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorSlidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
