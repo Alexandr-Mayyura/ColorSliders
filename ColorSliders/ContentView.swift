383e25c6ba74b810709470ad77956ad9308ee560
Alexandr-Mayyura <sonickill@yandex.ru>
2022-12-13 13:09:33 +0300

    ok work, but...

--- ColorSliders/ContentView.swift
+++ ColorSliders/ContentView.swift
@@ -12,12 +12,19 @@ struct ContentView: View {
     @State private var greenValue = Double(Int.random(in: 0...255))
     @State private var blueValue = Double(Int.random(in: 0...255))
     
+    @State private var redText = 0.0
+    @State private var greenText = 0.0
+    @State private var blueText = 0.0
+    
     @FocusState var isInputActive: NameColor?
+    @FocusState private var nameInFocus: Bool
+    
     @State private var isUpDown = false
+    @State private var alertPresented = false
     
     var body: some View {
         ZStack {
-            Color.blue
+            Color.cyan
                 .ignoresSafeArea()
             VStack(spacing: 20) {
                 Rectangle()
@@ -29,12 +36,37 @@ struct ContentView: View {
                     .frame(width: 350, height: 170)
                     .cornerRadius(16)
                     .overlay(RoundedRectangle(cornerRadius: 16).stroke(Color.white, lineWidth: 4))
-                ColorTools(value: $redValue, textLabel: "\(lround(redValue))")
+                ColorTools(
+                    value: $redValue,
+                    textLabel: "\(lround(redValue))",
+                    textTextfield: $redText,
+                    colorLine: .red
+                )
                     .focused($isInputActive, equals: .red)
-                ColorTools(value: $greenValue, textLabel: "\(lround(greenValue))")
+                    .onAppear {
+                        redText = redValue
+                    }
+                ColorTools(
+                    value: $greenValue,
+                    textLabel: "\(lround(greenValue))",
+                    textTextfield: $greenText,
+                    colorLine: .green
+                )
                     .focused($isInputActive, equals: .green)
-                ColorTools(value: $blueValue, textLabel: "\(lround(blueValue))")
+                    .onAppear {
+                        greenText = greenValue
+                    }
+                ColorTools(
+                    value: $blueValue,
+                    textLabel: "\(lround(blueValue))",
+                    textTextfield: $blueText,
+                    colorLine: .blue
+                )
                     .focused($isInputActive, equals: .blue)
+                    
+                    .onAppear {
+                        blueText = blueValue
+                    }
                 Spacer()
             }
             .keyboardType(.numberPad)
@@ -52,7 +84,12 @@ struct ContentView: View {
                     }
                     Spacer()
                     Button("Done") {
+                        changedTextfield()
                         isInputActive = nil
+                        nameInFocus = false
+                    }
+                    .alert("Wrong format", isPresented: $alertPresented, actions: {}) {
+                        Text("Enter your name")
                     }
                 }
             }
@@ -60,42 +97,84 @@ struct ContentView: View {
         }
     }
     
-    private func changedTextfieldDown(isOn: Bool) {
-        switch (isInputActive, isOn) {
-        case (.red, isOn):
-            isInputActive = isOn ? .green : .blue
-        case (.green, isOn):
-            isInputActive = isOn ? .blue : .red
-        default:
-            isInputActive = isOn ? .red : .green
+    private func changedTextfield() {
+        if redText > 255 || greenText > 255 || blueText > 255 {
+            alertPresented.toggle()
+           return
+        } else {
+            switch isInputActive{
+            case .red:
+                redValue = redText
+            case .green:
+                greenValue = greenText
+            default:
+                blueValue = blueText
+            }
         }
     }
+    
+    private func changedTextfieldDown(isOn: Bool) {
+        if redText > 255 || greenText > 255 || blueText > 255 {
+            alertPresented.toggle()
+           return
+        } else {
+            switch (isInputActive, isOn) {
+            case (.red, isOn):
+                isInputActive = isOn ? .green : .blue
+                redValue = redText
+                
+            case (.green, isOn):
+                isInputActive = isOn ? .blue : .red
+                greenValue = greenText
+                
+            default:
+                isInputActive = isOn ? .red : .green
+                blueValue = blueText
+                
+            }
+        }
+    }
+    
+//    private func changedTextfieldDown() {
+//
+//        if redText > 255 || greenText > 255 || blueText > 255 {
+//            alertPresented.toggle()
+//           return
+//        }
+//    }
 }
 
 struct ColorTools: View {
     @Binding var value: Double
-    let textLabel: String
+    var textLabel: String
+    @Binding var textTextfield: Double
+    let colorLine: Color
 
     var body: some View {
         HStack {
             Text(textLabel)
                 .foregroundColor(.white)
                 .frame(width: 35)
+            
             Slider(value: $value, in: 0...255, step: 1)
                 .animation(.easeInOut, value: value)
-            ColorTextfieldView(value: $value, textLabel: textLabel)
+                .accentColor(colorLine)
+                .onChange(of: value) { newValue in
+                    textTextfield = newValue
+                }
+            ColorTextfieldView(textLabel: $textTextfield)
         }
     }
 }
 
 struct ColorTextfieldView: View {
-    @Binding var value: Double
-    let textLabel: String
+    
+    @Binding var textLabel: Double
 
     var body: some View {
-        TextField(textLabel, value: $value, format: .number)
-            .frame(width: 50, height: 30)
+        TextField("", value: $textLabel, format: .number)
             .background(.white)
+            .frame(width: 50, height: 30)
             .cornerRadius(6)
             .textFieldStyle(.roundedBorder)
     }
